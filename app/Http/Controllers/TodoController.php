7ffc139b08eb35ef150b8a501da0af0928e256e8
Ekram <74317232+EkramAbdullahAraf@users.noreply.github.com>
2023-07-29 01:52:26 +0600

    Update TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,20 +1,49 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
-    public function login(Request $request)
+    public function index()
     {
-        $credentials = $request->only('email', 'password');
-
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-        return $this->respondWithToken($token);
+        $todos = Auth::user()->todos;
+        return response()->json(['todos' => $todos], 200);
     }
 
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        $todo = $user->todos()->create($request->all());
+        return response()->json(['todo' => $todo], 201);
+    }
+
+    public function show($id)
+    {
+        $todo = Auth::user()->todos()->find($id);
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+        return response()->json(['todo' => $todo], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Auth::user()->todos()->find($id);
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+        $todo->update($request->all());
+        return response()->json(['todo' => $todo], 200);
+    }
+
+    public function destroy($id)
+    {
+        $todo = Auth::user()->todos()->find($id);
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+        $todo->delete();
+        return response()->json(['message' => 'Todo deleted'], 200);
+    }
 }
